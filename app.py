aec72f2d0e56aed795a86fc6e22c971444aa23c5
muhammetsaltuk <muhammetsaltukozdemir@gmail.com>
2022-10-02 04:27:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 import pandas as pd
 
 app = Flask(__name__)
+CORS(app)
 
 
-@app.route("/")
-def getData():
+@app.route("/api/locations")
+def getLocations():
+    df = pd.read_csv("./collections/1979_sm_locations.csv")
+    return pd.DataFrame(df).to_json()
+
+@app.route("/api/craters")
+def getCreaters():
     df = pd.read_csv("./collections/1979_sm_locations.csv")
     return pd.DataFrame(df).to_json()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)

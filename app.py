1091bb8f6a68be6bc771bb6f2e67c0a81fb80d54
Şükrü Taha Bıyık <57368706+ultimate-dev@users.noreply.github.com>
2022-10-02 15:43:04 +0300

    ..

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, jsonify
+import json
+from flask import Flask
 from flask_cors import CORS
 import pandas as pd
+
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn import preprocessing
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 app = Flask(__name__)
 CORS(app)
@@ -15,6 +21,15 @@ def getLocations():
 def getCreaters():
     df = pd.read_csv("./collections/moon_features.csv")
     return pd.DataFrame(df).to_json()
+
+@app.route("/api/centers")
+def getKMeans():
+    centers = list([[-22.45454545, -64.54545455],
+       [ 30.63636364,  51.81818182],
+       [ 49.6       , -23.4       ]])
+    return centers
+   
+
 
 
 if __name__ == '__main__':
